e99d059511aca177dda667672650725c65ad2e00
Daniel McKenna <mckenna.daniel15@gmail.com>
2021-10-06 09:59:59 -0600

    finished assessment

---
+++ gitInfo.js
@@ -0,0 +1,116 @@
+/*
+    For this section of the assessment you will be putting together a cheat sheet for common git commands.  
+    You'll provide the command as well as what it does. 
+*/
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Create a variable called 'gitDefinition'.
+    It should be a string containing your best definition of what Git is.
+*/
+
+//CODE HERE
+
+let gitDefinition =
+  "its a version control system that can be used to save snippets of your code at different times/ versions";
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable called 'gitHubDefinition'.  
+    It should be a string containing your best definition of what GitHub is.
+*/
+
+//CODE HERE
+
+let gitHubDefinition =
+  "git Hub is the site or GUI of a product that uses git where you can save your snipets of code or repos of code too.";
+
+//////////////////PROBLEMS 3 - 8////////////////////
+/*
+    For the next several problems you will be creating objects containing information about different git 
+    commands.  Each object should contain 'description' and 'code' properties.  The 'description' property 
+    will be a string with a description of what that git command does.  The 'code' property should be a 
+    string of the actual command (what you type into your terminal).
+*/
+
+//////////////////PROBLEM 3////////////////////
+/*
+    Create an object called 'init' with 'description' and 'code' properties 
+    following the guidelines above to describe the init command.
+*/
+
+//CODE HERE
+
+const init = {
+  description:
+    "is the initalization of your project/repository it is when you are creating your first version of your project",
+  code: "git init",
+};
+
+//////////////////PROBLEM 4////////////////////
+/*
+    Create an object called 'clone' with 'description' and 'code' properties 
+    following the guidelines above to describe the clone command.
+*/
+
+//CODE HERE
+
+const clone = {
+  description:
+    "this will take an existing repository and create a clone either locally or remotly",
+  code: "git clone",
+};
+
+//////////////////PROBLEM 5////////////////////
+/*
+    Create an object called 'status' with 'description' and 'code' properties 
+    following the guidelines above to describe the status command.
+*/
+
+//CODE HERE
+const status = {
+  description: "it shows you what stage you are in on your working directory",
+  code: "git status",
+};
+
+//////////////////PROBLEM 6////////////////////
+/*
+    Create an object called 'add' with 'description' and 'code' properties 
+    following the guidelines above to describe the add command.
+*/
+
+//CODE HERE
+
+const add = {
+  description:
+    "adds all changes or untracked files to the staging area getting them ready for the next git commit",
+  code: "git add *file or directory you want to add to the staging process*",
+};
+
+//////////////////PROBLEM 7////////////////////
+/*
+    Create an object called 'commit' with 'description' and 'code' properties
+    following the guidelines above to describe the commit command.
+*/
+
+//CODE HERE
+
+const commit = {
+  description:
+    "takes a picture or saves a copy of your local repository at the time of the commit",
+  code: "git commit 'what you did'",
+};
+
+//////////////////PROBLEM 8////////////////////
+/*
+    Create an object called 'push' with 'description' and 'code' properties 
+    following the guidelines above to describe the push command.
+*/
+
+//CODE HERE
+
+const push = {
+  description:
+    "uploads all yoru commited local data in your repository to the remote repo",
+  code: "git push origin main",
+};
---
+++ sldc.js
@@ -0,0 +1,25 @@
+// In this section you will answer some work through problems related to the Software Development Lifecycle
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
+*/
+
+// Analysis of Requirements
+// Planning
+// Design
+// Implementation
+// Testing & Integration
+// Maintain
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
+*/
+
+let analysis = "Analysis of Requirements";
+let planning = "Planning";
+let design = "Design";
+let implement = "Implementation/ software development";
+let testing = "Testing what you build";
+let maintain = "maintain what you built";
